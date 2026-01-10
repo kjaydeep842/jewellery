ebf4e43653f743151d38853f0ce28ee80da5ed6e
Jaydeep Khunt <kjaydeep842@gmail.com>
2026-01-11 02:25:42 +0530

    .

--- jewelry.sql
+++ jewelry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2025 at 05:57 PM
+-- Generation Time: Jan 10, 2026 at 09:51 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `jewelry`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banners`
+--
+
+CREATE TABLE `banners` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `desc` text DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `title`, `desc`, `image`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'qeq', 'qeweq', 'banners/MBwEs8QwkxjSTCQOX1YDP5szxtUF8y49jWRbaveS.png', 1, '2025-11-27 12:53:47', '2026-01-08 10:25:42');
 
 -- --------------------------------------------------------
 
@@ -65,7 +88,8 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `slug`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Nackless', 'nackless', 'This is Nackless Category', '2025-11-21 10:59:24', '2025-11-21 10:59:24');
+(1, 'Gold', 'gold', 'This is Gold Category', '2025-11-21 10:59:24', '2025-11-26 10:05:18'),
+(2, 'qwe', 'qwe', 'qwe', '2026-01-10 14:47:10', '2026-01-10 14:47:10');
 
 -- --------------------------------------------------------
 
@@ -82,6 +106,31 @@ CREATE TABLE `failed_jobs` (
   `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `general_settings`
+--
+
+CREATE TABLE `general_settings` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `site_name` varchar(255) NOT NULL DEFAULT 'LuxeGems',
+  `primary_color` varchar(255) NOT NULL DEFAULT '#ffbf00',
+  `secondary_color` varchar(255) NOT NULL DEFAULT '#000000',
+  `header_color` varchar(255) DEFAULT '#ffffff',
+  `font_family` varchar(255) NOT NULL DEFAULT 'Cinzel',
+  `logo_path` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `general_settings`
+--
+
+INSERT INTO `general_settings` (`id`, `site_name`, `primary_color`, `secondary_color`, `header_color`, `font_family`, `logo_path`, `created_at`, `updated_at`) VALUES
+(1, 'Tattsvi', '#ebac00', '#000000', '#ffffff', 'Roboto', 'logos/4VeuYRb3I2DjljbGZS2P55cK6v3G2S9oYOcX9Xu3.png', '2026-01-10 12:50:59', '2026-01-10 13:50:40');
 
 -- --------------------------------------------------------
 
@@ -145,7 +194,18 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (8, '2025_11_21_155856_create_orders_table', 1),
 (9, '2025_11_21_160540_add_email_phone_to_orders_table', 1),
 (10, '2025_11_21_163100_add_category_id_to_products_table', 2),
-(11, '2025_11_21_164350_remove_old_category_column_from_products_table', 3);
+(11, '2025_11_21_164350_remove_old_category_column_from_products_table', 3),
+(12, '2025_11_26_145743_create_subcategories_table', 4),
+(13, '2025_11_26_145836_add_subcategory_id_to_products_table', 4),
+(14, '2025_11_26_145919_create_tags_table', 4),
+(16, '2025_11_26_150035_create_product_images_table', 4),
+(17, '2025_11_26_182157_create_product_tag_table', 5),
+(18, '2025_11_26_184246_add_status_to_tags_table', 6),
+(19, '2025_11_27_000000_create_banners_table', 7),
+(20, '2025_11_27_182227_create_banners_table', 8),
+(21, '2025_11_27_182622_create_personal_access_tokens_table', 9),
+(22, '2026_01_10_182029_create_general_settings_table', 10),
+(24, '2026_01_10_191236_add_header_color_to_general_settings_table', 11);
 
 -- --------------------------------------------------------
 
@@ -186,6 +246,34 @@ CREATE TABLE `password_reset_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `personal_access_tokens`
+--
+
+INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
+(1, 'App\\Models\\User', 2, 'auth_token', '711126c2b20d991cd9912184a05d881b0a08dcd0a2c8ee08d814664fa45dceca', '[\"*\"]', NULL, NULL, '2025-11-27 13:34:28', '2025-11-27 13:34:28'),
+(2, 'App\\Models\\User', 2, 'auth_token', 'fd7f119e5ac04728bb4d3554722ceb83c9f70e1590c6476ddc0def0c9e7d10d4', '[\"*\"]', NULL, NULL, '2025-11-27 13:35:53', '2025-11-27 13:35:53'),
+(3, 'App\\Models\\User', 3, 'auth_token', 'f363530abdf7c9d3d057a4e71a543ce8f9169e7ca85c2dc6a93f28e2d3b82ce1', '[\"*\"]', NULL, NULL, '2025-11-27 13:37:28', '2025-11-27 13:37:28');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -199,6 +287,7 @@ CREATE TABLE `products` (
   `sku` varchar(255) DEFAULT NULL,
   `stock` int(11) NOT NULL DEFAULT 0,
   `category_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `subcategory_id` bigint(20) UNSIGNED DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `gallery` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`gallery`)),
   `material` varchar(255) DEFAULT NULL,
@@ -212,8 +301,55 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `slug`, `description`, `price`, `discount_price`, `sku`, `stock`, `category_id`, `image`, `gallery`, `material`, `weight`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Golden Chainq', 'golden-chainq', 'weqwe', 999.00, NULL, NULL, 0, 1, 'products/PHPGKETTF9ItoazgLxN2J4Bsf2FFHMNdMx29aYy0.png', NULL, NULL, NULL, 'active', '2025-11-21 11:07:57', '2025-11-21 11:07:57');
+INSERT INTO `products` (`id`, `name`, `slug`, `description`, `price`, `discount_price`, `sku`, `stock`, `category_id`, `subcategory_id`, `image`, `gallery`, `material`, `weight`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Golden Chainq', 'golden-chainq', 'weqwe', 999.00, NULL, NULL, 0, 1, 1, 'products/PHPGKETTF9ItoazgLxN2J4Bsf2FFHMNdMx29aYy0.png', NULL, NULL, NULL, 'active', '2025-11-21 11:07:57', '2025-11-26 10:09:13'),
+(3, 'nackless', 'nackless', 'qw', 657567.00, NULL, NULL, 0, 1, 1, 'products/iMDne8o6e4bE0QIplPBnMRt6vzOxeKdiDVag8XXO.png', NULL, NULL, NULL, 'active', '2025-11-26 13:14:46', '2026-01-10 13:31:44'),
+(4, 'sadqwee', 'sadqwee', 'rwer', 3134.00, NULL, NULL, 0, 1, 1, 'products/Y8DmLOUmhoReJoMzhC0hGd1arAyFnfMrAmqGimoM.jpg', NULL, NULL, NULL, 'active', '2025-11-26 13:24:22', '2025-11-26 13:24:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `product_images`
+--
+
+INSERT INTO `product_images` (`id`, `product_id`, `image_path`, `created_at`, `updated_at`) VALUES
+(2, 3, 'products/gallery/8lkO7BJAwGTBHdf5aKpEmMwZWtSxWRuWYrO2tUNk.jpg', '2025-11-26 13:14:47', '2025-11-26 13:14:47'),
+(3, 4, 'products/gallery/HtLggBQxpGxEnksocjJ9fxXwOtz0OICpCndpe71C.png', '2025-11-26 13:24:22', '2025-11-26 13:24:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_tag`
+--
+
+CREATE TABLE `product_tag` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `tag_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `product_tag`
+--
+
+INSERT INTO `product_tag` (`id`, `product_id`, `tag_id`, `created_at`, `updated_at`) VALUES
+(1, 3, 1, '2025-11-26 13:14:46', '2025-11-26 13:14:46'),
+(2, 4, 1, '2025-11-26 13:24:22', '2025-11-26 13:24:22'),
+(3, 4, 2, '2025-11-26 13:24:22', '2025-11-26 13:24:22');
 
 -- --------------------------------------------------------
 
@@ -235,7 +371,53 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('9Z38zmOJu6w502zaG8ofdOlcskGCKbWlzgE1tz6w', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiSENUeW5ZQk13RzNzUTBjWGl5RjNOazRMa0RKT0NRZXBzSldRUk5HNSI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czoyNzoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2FkbWluIjt9czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MzQ6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9vcmRlcnMiO3M6NToicm91dGUiO3M6MTg6ImFkbWluLm9yZGVycy5pbmRleCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjI7fQ==', 1763743728);
+('ivqGpMnqQw4aiw0kzDIlfDNgLu1zNQhcHBgDM2cg', 2, '127.0.0.1', 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Mobile Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoidjgzR0FmdGV5MDN3YmlraDEwSW9YMEw5T25ucTNEbHBraUtTQW5kYiI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6NTA6Imh0dHA6Ly81OWMyMTA1MjEyOGUubmdyb2stZnJlZS5hcHAvcHJvZHVjdC1kZXRhaWxzIjtzOjU6InJvdXRlIjtzOjE1OiJwcm9kdWN0LmRldGFpbHMiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToyO30=', 1768075379),
+('mnVRgD0iNmyaZyatZcwnCFMHpqQX4nV6OB15SGOI', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoicXNCUWNLZUxwSW51c0w3bGJtTDRMY25LQkMzNFc0SklMdU12d0UwViI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9kYXNoYm9hcmQiO3M6NToicm91dGUiO3M6MTU6ImFkbWluLmRhc2hib2FyZCI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjI7fQ==', 1768078035);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategories`
+--
+
+CREATE TABLE `subcategories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `category_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `subcategories`
+--
+
+INSERT INTO `subcategories` (`id`, `category_id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Rose Gold', 'rose-gold', '2025-11-26 10:05:36', '2025-11-26 10:05:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`, `slug`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'chain', 'chain', 'active', '2025-11-26 12:59:04', '2025-11-26 12:59:04'),
+(2, 'erwer', 'erwer', 'active', '2025-11-26 13:23:49', '2025-11-26 13:23:49');
 
 -- --------------------------------------------------------
 
@@ -261,11 +443,18 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `is_admin`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Test User', 'test@example.com', 0, '2025-11-21 10:52:05', '$2y$12$THCmeoLkMQLxavy/QKoaCupKNvLzzQf6u167DtZKIggaT6vLtJpgW', 'J5fEskhfiq', '2025-11-21 10:52:06', '2025-11-21 10:52:06'),
-(2, 'Jaydeep Khunt', 'kjaydeep842@gmail.com', 1, NULL, '$2y$12$8yLoTHEFHK/2E.caobqnquLiZCEuvGKE3XA0pbsu/Ud/NtQNlLL76', NULL, '2025-11-21 10:53:04', '2025-11-21 10:53:04');
+(2, 'Jaydeep Khunt', 'kjaydeep842@gmail.com', 1, NULL, '$2y$12$8yLoTHEFHK/2E.caobqnquLiZCEuvGKE3XA0pbsu/Ud/NtQNlLL76', 'd5aeJhyOVcGJgxKXT1xbx8FAdU5KoasFBcnxCFChZUUc9Gq65OUKQ0Qs84b6', '2025-11-21 10:53:04', '2025-11-21 10:53:04'),
+(3, 'Jay Khunt', 'jaydeep@example.com', 0, NULL, '$2y$12$9bVwKmGdMvRiShFoEEvRFeQzAfO96bm7I4rJxZ2nkrHyUUoGAv/rG', NULL, '2025-11-27 13:37:28', '2025-11-27 13:37:28');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `cache`
@@ -292,6 +481,12 @@ ALTER TABLE `categories`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `general_settings`
+--
+ALTER TABLE `general_settings`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `jobs`
@@ -325,13 +520,38 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`),
+  ADD KEY `personal_access_tokens_expires_at_index` (`expires_at`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `products_slug_unique` (`slug`),
   ADD UNIQUE KEY `products_sku_unique` (`sku`),
-  ADD KEY `products_category_id_foreign` (`category_id`);
+  ADD KEY `products_category_id_foreign` (`category_id`),
+  ADD KEY `products_subcategory_id_foreign` (`subcategory_id`);
+
+--
+-- Indexes for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_images_product_id_foreign` (`product_id`);
+
+--
+-- Indexes for table `product_tag`
+--
+ALTER TABLE `product_tag`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uniq_product_tag` (`product_id`,`tag_id`),
+  ADD KEY `fk_product_tag_tag` (`tag_id`);
 
 --
 -- Indexes for table `sessions`
@@ -340,6 +560,22 @@ ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
   ADD KEY `sessions_user_id_index` (`user_id`),
   ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indexes for table `subcategories`
+--
+ALTER TABLE `subcategories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subcategories_slug_unique` (`slug`),
+  ADD KEY `subcategories_category_id_foreign` (`category_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tags_name_unique` (`name`),
+  ADD UNIQUE KEY `tags_slug_unique` (`slug`);
 
 --
 -- Indexes for table `users`
@@ -353,16 +589,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `banners`
+--
+ALTER TABLE `banners`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `general_settings`
+--
+ALTER TABLE `general_settings`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `jobs`
@@ -374,7 +622,7 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -383,16 +631,46 @@ ALTER TABLE `orders`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `product_images`
+--
+ALTER TABLE `product_images`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `product_tag`
+--
+ALTER TABLE `product_tag`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `subcategories`
+--
+ALTER TABLE `subcategories`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -402,7 +680,27 @@ ALTER TABLE `users`
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `products_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `products_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `products_subcategory_id_foreign` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategories` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD CONSTRAINT `product_images_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `product_tag`
+--
+ALTER TABLE `product_tag`
+  ADD CONSTRAINT `fk_product_tag_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_product_tag_tag` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `subcategories`
+--
+ALTER TABLE `subcategories`
+  ADD CONSTRAINT `subcategories_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
